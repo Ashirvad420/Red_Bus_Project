c53ebe211be330eda4421a06fac6fea72a0be720
ashirvadojha <ashirvadojha123@gmail.com>
2024-03-02 16:00:48 +0530

    Bus Reservation Portal Using backend

---
+++ src/main/java/com/app/B.java
@@ -0,0 +1,14 @@
+package com.app;
+
+public class B {
+
+    public static void main(String[] args) {
+
+        A a1 = new A(1,"ashirvad","patna");
+        System.out.println(a1);
+    }
+}
+
+/*
+    Tostring printing the content of object address.. it is not printing object
+ */
---
+++ src/main/java/com/app/exceptionhandler/ErrorDetails.java
@@ -0,0 +1,62 @@
+package com.app.exceptionhandler;
+
+import java.time.LocalDateTime;
+
+public class ErrorDetails {
+	
+	private LocalDateTime timestamp;
+	private String message;
+	private String details;
+
+	public ErrorDetails(String message, LocalDateTime now, String description) {
+	}
+
+
+	public LocalDateTime getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(LocalDateTime timestamp) {
+		this.timestamp = timestamp;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getDetails() {
+		return details;
+	}
+	public void setDetails(String details) {
+		this.details = details;
+	}
+	
+	
+	public ErrorDetails(LocalDateTime timestamp, String message, String details) {
+		super();
+		this.timestamp = timestamp;
+		this.message = message;
+		this.details = details;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "ErrorDetails [timestamp=" + timestamp + ", message=" + message + ", details=" + details + "]";
+	}
+	
+	public ErrorDetails() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
+}
